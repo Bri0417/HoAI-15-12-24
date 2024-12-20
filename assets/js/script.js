e9d296a7383e2a987c4993fd62265c8334b2f12e
Bri0417 <brindha17neha@gmail.com>
2024-12-20 22:10:56 +0530

    what-we-do page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,26 @@
   });
 })(jQuery);
 
+// what we do tab
+
+(function($){
+  $(document).ready(function () {
+    // When a tab button is clicked
+    $('.tab-btn').on('click', function () {
+        // Remove 'active' class from all tabs and hide all tab contents
+        $('.tab-btn').removeClass('active');
+        $('.tab-content').removeClass('active');
+
+        // Add 'active' class to the clicked tab
+        $(this).addClass('active');
+
+        // Get the target tab content's ID
+        const targetTab = $(this).data('tab');
+
+        // Show the corresponding content
+        $('#' + targetTab).addClass('active');
+    });
+});
+
+})(jQuery);
+
